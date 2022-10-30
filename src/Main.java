4097aed50799cdf8a68b7c915955f8f8fc4024ab
Angshuman Borah <angshumaanborah@gmail.com>
2022-10-30 21:46:45 +0530

    Codesignal inro 16 Solved

--- src/Main.java
+++ src/Main.java
@@ -8,8 +8,10 @@ public class Main {
 		//System.out.println(codePractise.reverseInParentheses1("foo(bar(baz))blim"));
 
         int arr[]={1,2};
-		System.out.println(codePractise.findMssingElement(arr, 3));
-		
+		//System.out.println(codePractise.findMssingElement(arr, 3));
+		int arr1[]={1,10,13};
+		int arr2[]={10,1,13};
+		System.out.println(new AreSimilar().solution(arr1, arr2));
 		
 	}
 	
